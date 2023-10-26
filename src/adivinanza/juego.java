6ec547c4337a6f5e338beb4db0c66f3828e0d846
unknown <migliofranco51@gmail.com>
2023-10-26 11:23:18 -0300

    1erTP

--- src/adivinanza/juego.java
+++ src/adivinanza/juego.java
@@ -12,7 +12,7 @@ public class juego {
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
 
-		int numintentos  = 0;
+		int numintentos = 0;
 		int numeroIngresado;
 		int numeroSecreto;
 
@@ -22,40 +22,39 @@ public class juego {
 			try {
 				System.out.print("Ingresa un número entre 1 y 100: ");
 				if (scanner.hasNextInt()) {
-					
+
 					numeroIngresado = scanner.nextInt();
-					
+
 					validarNumero(numeroIngresado);
 
 					numintentos++;
 
 					if (numintentos == 10) {
-						
+
 						System.out.println("Se ha alcanzado el número máximo de intentos");
 						break;
 					}
 
 					if (numeroIngresado < numeroSecreto) {
 						System.out.println("El número secreto es mayor.");
-						
+
 					} else if (numeroIngresado > numeroSecreto) {
 						System.out.println("El número secreto es menor.");
 					} else {
-						System.out.println(
-								"¡Correcto! Has adivinado el número secreto en " + numintentos + " intentos.");
+						System.out
+								.println("¡Correcto! Has adivinado el número secreto en " + numintentos + " intentos.");
 						break;
-						
+
 					}
-					
+
 				} else {
 					System.out.println("Error: Debes ingresar un número válido.");
-					
-					
+
 					scanner.nextLine();
 				}
 			} catch (IllegalArgumentException e) {
-				
-				System.out.println(e.getMessage() );
+
+				System.out.println(e.getMessage());
 			}
 		} while (true);
 
@@ -63,9 +62,9 @@ public class juego {
 	}
 
 	private static void validarNumero(int numero) {
-	
+
 		if (numero < 1 || numero > 100) {
-			
+
 			throw new IllegalArgumentException("El número debe estar entre 1 y 100.");
 		}
 	}
